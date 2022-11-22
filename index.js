c85eb78fb23913d629373414de8e67c6358c9a6d
Sabrina <sabrina.wong@cix.csi.cuny.edu>
2022-11-22 09:13:53 -0500

    commit tree d2efd9faa3b4305b327c7c1bca91b24d268ecb5a

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function saturdayFun (){
+    console.log ('roller-skate');
+}
+
+const mondayWork = function(){
+    console.log ('go to the office');
+
+}
+
+wrapAdjective ('*')('highlight');
